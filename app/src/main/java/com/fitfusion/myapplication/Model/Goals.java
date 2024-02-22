c8ab67bca009ea4e79ecd2839793516e472b5f2b
HoXanh <hoxuananh543@gmail.com>
2024-02-22 16:38:04 +0000

    Setup Android Studio Setup Firebase Create date model for Users and Goals

---
+++ app/src/main/java/com/fitfusion/myapplication/Model/Goals.java
@@ -0,0 +1,12 @@
+package com.fitfusion.myapplication.Model;
+
+import java.util.ArrayList;
+
+public class Goals {
+    private String fitnessGoal;  // muscle gain, weight loss
+    private String bodyType;     // Skinny, Fat
+    private Boolean physicalWork; // Is your current job physical? Answer: yes or no;
+    private String trainingDays;  // 1 day per week, 3 days per week, 5 days per week
+    private String exerciseMotivation; // to gain strength, improving cardiovascular health, injury
+
+}
