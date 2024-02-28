bc2e9204c1f4e27e7e3bc48748f8665b4cfea9ae
bavornkiet <you@example.com>
2024-02-28 22:57:58 +0000

    Planner stuff

--- app/src/main/java/com/fitfusion/myapplication/Planner.java
+++ app/src/main/java/com/fitfusion/myapplication/Planner.java
@@ -3,6 +3,7 @@ package com.fitfusion.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Planner extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class Planner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planner);
+    }
+
+    public void LosingWeight(View view){
+
+    }
+    public void GainMuscle(View view) {
     }
 }
