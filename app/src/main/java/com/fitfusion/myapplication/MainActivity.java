e939493abe8cf9f9f986d43035a450cfc210b6db
bavornkiet <you@example.com>
2024-03-01 16:54:57 +0000

    Planner

--- app/src/main/java/com/fitfusion/myapplication/MainActivity.java
+++ app/src/main/java/com/fitfusion/myapplication/MainActivity.java
@@ -7,21 +7,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button plannerBtn1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.fitnessPg:
+                    // Intent to go to Home Activity
+                    startActivity(new Intent(MainActivity.this, Planner.class));
+                    return true;
+                case R.id.foodPg:
+                    // Intent to go to Dashboard Activity
+                    startActivity(new Intent(MainActivity.this, Planner.class));
+                    return true;
+                case R.id.blogPg:
+                    // Intent to go to Notifications Activity
+                    startActivity(new Intent(MainActivity.this, Blog.class));
+                    return true;
 
-        plannerBtn1 = findViewById(R.id.plannerBtn);
+                case R.id.profilePg:
+                    startActivity(new Intent(MainActivity.this, Profile.class));
+                    return true;
 
-        plannerBtn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Planner.class));
+                case R.id.homePg:
+                    startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    return true;
             }
+            return false;
         });
+
+
     }
 }
--- app/src/main/java/com/fitfusion/myapplication/SecondActivity.java
+++ app/src/main/java/com/fitfusion/myapplication/SecondActivity.java
@@ -2,13 +2,34 @@ package com.fitfusion.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button plannerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        plannerBtn = findViewById(R.id.plannerBtn);
+        plannerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, Planner.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+        
+    }
+
+    public void Imagebtnclicked(View view) {
+
     }
 }
